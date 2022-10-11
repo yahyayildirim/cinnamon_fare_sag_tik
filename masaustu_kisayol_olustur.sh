10e253070aa9722fcc7a94b97e97caeb3acceb5b
Yahya YILDIRIM <yahya.yldrm@gmail.com>
2022-10-11 13:33:12 +0000

    kırık link hatası düzeltildi.

--- masaustu_kisayol_olustur.sh
+++ masaustu_kisayol_olustur.sh
@@ -11,9 +11,9 @@ if [ -e "$DEST/$LINK".desktop ]; then
   notify-send --expire-time=200000 "Bağlantı oluşturulurken hata oluştu. $DEST/$LINK zaten var. Tekrar deneyin."
   exit 1
 fi
-#echo -e "[Desktop Entry]\nName=$LINK\nURL=$1\nComment=\nTerminal=false\nIcon=folder\nType=Link" > "$DEST/$LINK".desktop
-#chmod u+x "$DEST/$LINK".desktop
-ln -sfr $LINK $DEST
+echo -e "[Desktop Entry]\nName=$LINK\nURL=$1\nComment=\nTerminal=false\nIcon=folder\nType=Link" > "$DEST/$LINK".desktop
+chmod u+x "$DEST/$LINK".desktop
+#ln -sfr $LINK $DEST
 
 if [ $? = 1 ]; then
     notify-send --expire-time=200000 "Bağlantı oluşturulurken hata oluştu. Tekrar deneyin."
